b038ae3348140141aa824bd8f832d23202629b09
vihaan <vihaanverama24@gmail.com>
2018-10-30 15:15:09 +0530

    swipe listeners added

--- app/src/main/java/com/lottie/demo/MainActivity.kt
+++ app/src/main/java/com/lottie/demo/MainActivity.kt
@@ -2,11 +2,56 @@ package com.lottie.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Window
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , SwipeListener{
+    val TAG = MainActivity@this.javaClass.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideActionbar()
         setContentView(R.layout.activity_main)
+        init()
     }
+
+    private fun init() {
+        initSwipeListener()
+    }
+
+    private fun hideActionbar() {
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        supportActionBar?.hide();
+    }
+
+    private lateinit var listener: OnSwipeTouchListener
+    private fun initSwipeListener() {
+        listener = OnSwipeTouchListener(this, this)
+        rootCL.setOnTouchListener(listener)
+    }
+
+    var position = 0;
+    override fun onSwipeLeft() {
+        Log.d(TAG, "left swipe")
+        if(position >0)
+        {
+        }
+    }
+
+    override fun onSwipeRight() {
+        Log.d(TAG, "right swipe")
+    }
+
+    override fun onSwipeTop() {
+
+        Log.d(TAG, "top swipe")
+    }
+
+    override fun onSwipeBottom() {
+
+        Log.d(TAG, "bottom swipe")
+    }
+
+
 }
---
+++ app/src/main/java/com/lottie/demo/SwipListener.kt
@@ -0,0 +1,8 @@
+package com.lottie.demo
+
+interface SwipeListener {
+    fun onSwipeLeft()
+    fun onSwipeRight()
+    fun onSwipeTop()
+    fun onSwipeBottom()
+}
